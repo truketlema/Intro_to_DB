7b61dbc391e434441cfe757fdf02188e56e130dc
Truket Lema <truketlema9489@gmail.com>
2025-11-01 02:04:27 +0300

    corrections

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (1, 'Cole Baidoo', 'cbdaioo@sandtech.com', '123 Hapiness Ave')
