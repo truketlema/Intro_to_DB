6dc7acc33e9b35e7c59dfdb8da7a33a1f71cc934
Truket Lema <truketlema9489@gmail.com>
2025-11-01 02:06:16 +0300

    done

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(1, 'Cole Baidoo', 'cbdaioo@sandtech.com', '123 Hapiness Ave')
+(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Hapiness Ave.")
